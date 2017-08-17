21027ab024845f3f0f42a98eb6acb6e24a0c51cd
Federico Tomassetti <federico@tomassetti.me>
2017-08-17 09:33:20 +0200

    start modeling inference process

---
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/TIType.java
@@ -0,0 +1,17 @@
+package com.github.javaparser.symbolsolver.resolution.typeinference;
+
+import com.github.javaparser.symbolsolver.model.typesystem.Type;
+
+/**
+ * The term "type" is used loosely in this chapter to include type-like syntax that contains inference variables.
+ * The term proper type excludes such "types" that mention inference variables. Assertions that involve inference
+ * variables are assertions about every proper type that can be produced by replacing each inference variable with
+ * a proper type.
+ */
+public abstract class TIType {
+
+    class ProperType extends TIType {
+        private Type type;
+    }
+
+}
