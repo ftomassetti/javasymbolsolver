ff16ae0203daddf5dbf7b60c1d044eedf6ae5a27
Federico Tomassetti <federico@tomassetti.me>
2017-08-17 12:34:03 +0200

    implementing some tests on inference

--- java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/TypeHelper.java
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/TypeHelper.java
@@ -1,7 +1,9 @@
 package com.github.javaparser.symbolsolver.resolution.typeinference;
 
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.symbolsolver.model.typesystem.ReferenceType;
 import com.github.javaparser.symbolsolver.model.typesystem.Type;
+import com.github.javaparser.symbolsolver.model.typesystem.Wildcard;
 
 /**
  * The term "type" is used loosely in this chapter to include type-like syntax that contains inference variables.
@@ -16,7 +18,22 @@ public class TypeHelper {
      * The term proper type excludes such "types" that mention inference variables.
      */
     public static boolean isProperType(Type type) {
-        return !(type instanceof InferenceVariable);
+        if (type instanceof InferenceVariable) {
+            return false;
+        }
+        if (type instanceof ReferenceType) {
+            ReferenceType referenceType = (ReferenceType) type;
+            return referenceType.typeParametersValues().stream().allMatch(it -> isProperType(it));
+        }
+        if (type instanceof Wildcard) {
+            Wildcard wildcard = (Wildcard)type;
+            if (wildcard.isBounded()) {
+                return isProperType(wildcard.getBoundedType());
+            } else {
+                return true;
+            }
+        }
+        throw new UnsupportedOperationException(type.toString());
     }
 
     public static boolean isCompatibleInALooseInvocationContext(Expression expression, Type t) {
