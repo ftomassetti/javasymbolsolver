6cb45753f777696840fa830b2b8c0f4cebe05e4c
Federico Tomassetti <federico@tomassetti.me>
2017-08-17 17:12:41 +0200

    working on TypeInferenceAPI

---
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/InstantiationSet.java
@@ -0,0 +1,7 @@
+package com.github.javaparser.symbolsolver.resolution.typeinference;
+
+public class InstantiationSet {
+    public boolean allInferenceVariablesAreResolved(BoundSet boundSet) {
+        throw new UnsupportedOperationException();
+    }
+}
