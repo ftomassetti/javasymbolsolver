b2bf834e9539826ca2b324035c1c5ea3a32b1587
Federico Tomassetti <federico@tomassetti.me>
2017-08-18 09:55:22 +0200

    progress on loose compatibility

--- java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/TypeHelper.java
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/TypeHelper.java
@@ -1,9 +1,14 @@
 package com.github.javaparser.symbolsolver.resolution.typeinference;
 
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.model.typesystem.PrimitiveType;
 import com.github.javaparser.symbolsolver.model.typesystem.ReferenceType;
 import com.github.javaparser.symbolsolver.model.typesystem.Type;
 import com.github.javaparser.symbolsolver.model.typesystem.Wildcard;
+
+import java.util.Optional;
 
 /**
  * The term "type" is used loosely in this chapter to include type-like syntax that contains inference variables.
@@ -36,11 +41,98 @@ public class TypeHelper {
         throw new UnsupportedOperationException(type.toString());
     }
 
-    public static boolean isCompatibleInALooseInvocationContext(Expression expression, Type t) {
+    /**
+     * see https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.3
+     * @param expression
+     * @param t
+     * @return
+     */
+    public static boolean isCompatibleInAStrictInvocationContext(Expression expression, Type t) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * see https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.3
+     * @param expression
+     * @param t
+     * @return
+     */
+    public static boolean isCompatibleInALooseInvocationContext(TypeSolver typeSolver, Expression expression, Type t) {
+        //throw new UnsupportedOperationException("Unable to determine if " + expression + " is compatible in a loose invocation context with type " + t);
+        return isCompatibleInALooseInvocationContext(JavaParserFacade.get(typeSolver).getType(expression), t);
+    }
+
+    /**
+     * see https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.3
+     * @param s
+     * @param t
+     * @return
+     */
     public static boolean isCompatibleInALooseInvocationContext(Type s, Type t) {
+        // Loose invocation contexts allow a more permissive set of conversions, because they are only used for a
+        // particular invocation if no applicable declaration can be found using strict invocation contexts. Loose
+        // invocation contexts allow the use of one of the following:
+        //
+        // - an identity conversion (§5.1.1)
+
+        if (s.equals(t)) {
+            return true;
+        }
+
+        // - a widening primitive conversion (§5.1.2)
+
+        if (s.isPrimitive() && t.isPrimitive() && areCompatibleThroughWideningPrimitiveConversion(s, t)) {
+            return true;
+        }
+
+        // - a widening reference conversion (§5.1.5)
+
+        if (s.isReferenceType() && t.isReferenceType() && areCompatibleThroughWideningReferenceConversion(s, t)) {
+            return true;
+        }
+
+        // - a boxing conversion (§5.1.7) optionally followed by widening reference conversion
+
+        if (s.isPrimitive() && t.isReferenceType() &&
+                areCompatibleThroughWideningReferenceConversion(toBoxedType(s.asPrimitive()), t)) {
+            return true;
+        }
+
+        // - an unboxing conversion (§5.1.8) optionally followed by a widening primitive conversion
+
+        if (s.isReferenceType() && t.isPrimitive() &&
+                areCompatibleThroughWideningPrimitiveConversion(toUnboxedType(s.asReferenceType()), t)) {
+            return true;
+        }
+
+        // If, after the conversions listed for an invocation context have been applied, the resulting type is a raw
+        // type (§4.8), an unchecked conversion (§5.1.9) may then be applied.
+        //
+        // A value of the null type (the null reference is the only such value) may be assigned to any reference type
+        if (s.isNull() && t.isReferenceType()) {
+            return true;
+        }
+
+        throw new UnsupportedOperationException("isCompatibleInALooseInvocationContext unable to decide on s=" + s + ", t=" + t);
+    }
+
+    private static Type toUnboxedType(ReferenceType referenceType) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static Type toBoxedType(PrimitiveType primitiveType) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static boolean areCompatibleThroughWideningReferenceConversion(Type s, Type t) {
+        Optional<PrimitiveType> correspondingPrimitiveTypeForS = PrimitiveType.ALL.stream().filter(pt -> pt.getBoxTypeQName().equals(s.asReferenceType().getQualifiedName())).findFirst();
+        if (!correspondingPrimitiveTypeForS.isPresent()) {
+            return false;
+        }
+        throw new UnsupportedOperationException("areCompatibleThroughWideningReferenceConversion s="+s+", t=" + t);
+    }
+
+    private static boolean areCompatibleThroughWideningPrimitiveConversion(Type s, Type t) {
         throw new UnsupportedOperationException();
     }
 
